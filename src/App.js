fe300c80f6773f8ce973325e76357384f43f8562
CodingwithJess <thejessiebess@gmail.com>
2020-11-21 16:17:51 -0700

    added Recipe component and passed props through from App state to populate requested information from API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import Recipe from "./Recipe"
 import './App.css';
 
 const App = () => {
@@ -12,9 +13,11 @@ const App = () => {
   }, [])
 
   const getRecipes = async () => {
-    const response = await fetch (`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
+    console.log(data.hits)
   }
 
   return (
@@ -23,6 +26,13 @@ const App = () => {
         <input className="search-bar" type="text"/>
         <button className="search-button" type="submit">Search</button>
       </form>
+      {recipes.map(recipe => (
+        <Recipe 
+        title={recipe.recipe.label}
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}  
+        />
+      ))}
     </div>
   );
 }
